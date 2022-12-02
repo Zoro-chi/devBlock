a881a358f3e513fcdeaf545d896607a88a238976
Zoro-chi <jagbetuyi001@gmail.com>
2022-12-02 17:20:45 +0100

    socket config for deploy

--- client/src/Api/convoRequests.js
+++ client/src/Api/convoRequests.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 const getBaseUrl = () => {
-  let url;
-  switch (process.env.NODE_ENV) {
-    case "production":
-      url = "https://dev-block-socket.onrender.com/"
-      break;
+	let url;
+	switch (process.env.NODE_ENV) {
+		case "production":
+			url = "https://dev-block-api.onrender.com";
+			break;
 
-    case "development":
-    default:
-      url = "http://localhost:2121";
-  }
-  return url;
+		case "development":
+		default:
+			url = "http://localhost:2121";
+	}
+	return url;
 };
 
 const API = axios.create({
-  baseURL: getBaseUrl(),
+	baseURL: getBaseUrl(),
 });
 
 export const getConversations = (userId) =>
-  API.get(`/api/conversation/${userId}`);
+	API.get(`/api/conversation/${userId}`);
 
 export const getMessages = (chatId) => API.get(`/api/message/${chatId}`);
 
@@ -28,4 +28,4 @@ export const newMsgSend = (message) => API.post(`/api/message`, message);
 export const getFriends = (id) => API.get(`/api/users/friends/${id}`);
 
 export const getTwoUsersConvo = (userId1, userId2) =>
-  API.get(`/api/conversation/find/${userId1}/${userId2}`);
+	API.get(`/api/conversation/find/${userId1}/${userId2}`);
--- socket/index.js
+++ socket/index.js
@@ -1,18 +1,22 @@
 const httpServer = require("http").createServer();
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 // ! FOR DEV ENV
 // const io = require("socket.io")(process.env.PORT || 2244, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//   },
+// 	cors: {
+// 		origin: "http://localhost:3000",
+// 	},
 // });
 
-const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: process.env.CLIENT_URL,
-  },
-  path: process.env.API_URL,
+const io = new Server({
+	cors: {
+		origin: [
+			"https://dev-block.onrender.com/",
+			"https://dev-block-api.onrender.com/",
+		],
+	},
+	// path: process.env.API_URL,
 });
 
 console.log(process.env.SOCKET_URL);
@@ -20,48 +24,49 @@ console.log(process.env.SOCKET_URL);
 let users = [];
 
 const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+	!users.some((user) => user.userId === userId) &&
+		users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+	users = users.filter((user) => user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+	return users.find((user) => user.userId === userId);
 };
 
 io.on("connection", (socket) => {
-  // LOGGING FOR CONNECTION VERIFICATION
-  console.log("new user connected");
+	// LOGGING FOR CONNECTION VERIFICATION
+	console.log("new user connected");
 
-  // COLLECT USERID AND SOCKETID FROM USER
-  socket.on("sendUser", (userId) => {
-    addUser(userId, socket.id);
-    // SEND USERS TO FE(CLIENT-SIDE)
-    io.emit("getUsers", users);
-  });
+	// COLLECT USERID AND SOCKETID FROM USER
+	socket.on("sendUser", (userId) => {
+		addUser(userId, socket.id);
+		// SEND USERS TO FE(CLIENT-SIDE)
+		io.emit("getUsers", users);
+	});
 
-  // SEND AND GET MESSAGES
-  socket.on("sendMsg", ({ senderId, receiverId, txt }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMsg", {
-      senderId,
-      txt,
-    });
-  });
+	// SEND AND GET MESSAGES
+	socket.on("sendMsg", ({ senderId, receiverId, txt }) => {
+		const user = getUser(receiverId);
+		io.to(user.socketId).emit("getMsg", {
+			senderId,
+			txt,
+		});
+	});
 
-  socket.on("disconnect", () => {
-    // LOGGING FOR DIS-CONNECTION VERIFICATION
-    console.log("user disconnected");
-    removeUser(socket.id);
-    // SEND USERS TO FE(CLIENT-SIDE)
-    io.emit("getUsers", users);
-  });
+	socket.on("disconnect", () => {
+		// LOGGING FOR DIS-CONNECTION VERIFICATION
+		console.log("user disconnected");
+		removeUser(socket.id);
+		// SEND USERS TO FE(CLIENT-SIDE)
+		io.emit("getUsers", users);
+	});
 });
 
 const PORT = process.env.PORT || 2244;
-httpServer.listen(PORT, () => {
-  console.log(`Socket server running on ${PORT}`);
+
+io.listen(PORT, () => {
+	console.log(`Socket server running on ${PORT}`);
 });
