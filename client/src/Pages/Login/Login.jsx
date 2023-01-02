3832e8e0430edfcd94877e4f2ac41f788ef3d68f
Zoro-chi <jagbetuyi001@gmail.com>
2023-01-02 18:59:03 +0100

    prod settings

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -9,78 +9,63 @@ import { useAuthContext } from "../../context/authContext";
 import "./login.scss";
 
 const Login = () => {
-  const email = useRef();
-  const password = useRef();
-  const { user, isFetching, error, dispatch } = useAuthContext();
+	const email = useRef();
+	const password = useRef();
+	const { user, isFetching, error, dispatch } = useAuthContext();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    loginCall(
-      { email: email.current.value, password: password.current.value },
-      dispatch
-    );
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+	};
 
-  const github = (e) => {
-    e.preventDefault();
-    window.open("http://localhost:2121/api/auth/github", "_self");
-    getGithubUser(dispatch);
-  };
+	const github = (e) => {
+		e.preventDefault();
+		window.open("https://dev-block-api.onrender.com/api/auth/github/callback", "_self");
+		getGithubUser(dispatch);
+	};
 
-  console.log(user);
+	console.log(user);
 
-  return (
-    <div className="login">
-      <div className="login-wrapper">
-        <div className="login-left">
-          <h3 className="login-logo"> devBlock </h3>
-          <span className="login-desc">
-            Connect with different developers around the globe!
-          </span>
-        </div>
+	return (
+		<div className="login">
+			<div className="login-wrapper">
+				<div className="login-left">
+					<h3 className="login-logo"> devBlock </h3>
+					<span className="login-desc">Connect with different developers around the globe!</span>
+				</div>
 
-        <div className="login-right">
-          <form className="login-box-form" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              placeholder="Email"
-              className="login-input"
-              ref={email}
-              required
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="login-input"
-              ref={password}
-              required
-              minLength={6}
-            />
-            <button
-              className="login-button"
-              type="submit"
-              disabled={isFetching}
-            >
-              {isFetching ? <CircularProgress size="20px" /> : "Log In"}
-            </button>
-            <span className="or-span"> OR </span>
-            <button className="login-button" type="button" onClick={github}>
-              <GitHubIcon />
-            </button>
+				<div className="login-right">
+					<form className="login-box-form" onSubmit={handleSubmit}>
+						<input type="email" placeholder="Email" className="login-input" ref={email} required />
+						<input
+							type="password"
+							placeholder="Password"
+							className="login-input"
+							ref={password}
+							required
+							minLength={6}
+						/>
+						<button className="login-button" type="submit" disabled={isFetching}>
+							{isFetching ? <CircularProgress size="20px" /> : "Log In"}
+						</button>
+						<span className="or-span"> OR </span>
+						<button className="login-button" type="button" onClick={github}>
+							<GitHubIcon />
+						</button>
 
-            <Link to={"/register"} style={{ textDecoration: "none" }}>
-              <p className="register">Create a new account</p>
-            </Link>
-            {error && (
-              <div className="error" style={{ color: "red" }}>
-                {error}
-              </div>
-            )}
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+						<Link to={"/register"} style={{ textDecoration: "none" }}>
+							<p className="register">Create a new account</p>
+						</Link>
+						{error && (
+							<div className="error" style={{ color: "red" }}>
+								{error}
+							</div>
+						)}
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
